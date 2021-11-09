2c2a2baf0ec31a6961d44523dce256044e481d1a
Jessica Miller-Nims <Jessicamiller-nims@quickenloans.com>
2021-11-09 11:10:06 -0500

    updated media_type in apod

--- CoolSpaceProject/Models/Apod.cs
+++ CoolSpaceProject/Models/Apod.cs
@@ -14,7 +14,7 @@ namespace CoolSpaceProject.Models
         public string date { get; set; }
         public string explanation { get; set; }
         public string hdurl { get; set; }
-        public string media { get; set; }
+        public string media_type { get; set; }
         public string service_version { get; set; }
         public string title { get; set; }
         public string url { get; set; }
