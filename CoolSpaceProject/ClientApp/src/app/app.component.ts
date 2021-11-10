9290a98a58a12659018448a714f2e0f3f91cd1e7
Adam Rolain <AdamRolain@QuickenLoans.com>
2021-11-10 15:18:58 -0500

    Created apod component and added youtube player

--- CoolSpaceProject/ClientApp/src/app/app.component.ts
+++ CoolSpaceProject/ClientApp/src/app/app.component.ts
@@ -1,26 +1,12 @@
 import { Component } from '@angular/core';
-import { APOD } from './apod';
-import { CoolSpaceService } from './cool-space.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [ CoolSpaceService ]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'ClientApp';
-  testApod?: APOD;
 
-  constructor(private spaceservice: CoolSpaceService) {}
-
-  getApod() {
-    //console.log(`Current Date: ${this.testApod.date}`);
-    this.spaceservice.displayApod(
-      (result: any) => {
-        this.testApod = result;
-      }
-    );
-    //console.log(`New Date: ${this.testApod.date}`);
-  }
+  constructor() {}
 }
