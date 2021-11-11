d31f5db8cfb901a7eb18ab5f001d5c95aba44aba
Adam Rolain <AdamRolain@QuickenLoans.com>
2021-11-11 12:24:49 -0500

    Added enpoint to get currentUserId from UserDAL to frontend.

--- CoolSpaceProject/ClientApp/src/app/app.component.ts
+++ CoolSpaceProject/ClientApp/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +9,20 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class AppComponent {
   title = 'ClientApp';
+  currentUserId: number = -1;
 
-  constructor(private modalService: NgbModal) {}
+  constructor(private modalService: NgbModal, private userService: UserService) {}
 
   public open(modal: any): void {
     this.modalService.open(modal);
+  }
+
+  getCurrentUserId() {
+    this.userService.getCurrentUserId(
+      (result: any) => {
+        console.log(`Result: ${result}`)
+        this.currentUserId = result;
+      }
+    );
   }
 }
