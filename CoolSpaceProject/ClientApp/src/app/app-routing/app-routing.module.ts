ad848a09a298576a189d548d548a3a24f12947d0
Adam Rolain <AdamRolain@QuickenLoans.com>
2021-11-12 10:44:57 -0500

    Added in routing and completed endpoint for favoriteApodList

---
+++ CoolSpaceProject/ClientApp/src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { APODComponent } from '../apod/apod.component';
+import { ApodListComponent } from '../apod-list/apod-list.component';
+import { UserFormComponent } from '../user-form/user-form.component';
+import { UserLoginComponent } from '../user-login/user-login.component';
+import { MarsRoverComponent } from '../mars-rover/mars-rover.component';
+import { AppComponent } from '../app.component';
+
+
+const routes: Routes = [
+  // { path: 'home', component: AppComponent },
+  { path: 'apod', component: APODComponent },
+  { path: 'apodList', component: ApodListComponent },
+  { path: 'favoriteApodList', component: ApodListComponent, data: { isFavoriteList: true } },
+  { path: 'userSignUp', component: UserFormComponent },
+  { path: 'updateUser', component: UserFormComponent },
+  { path: 'login', component: UserLoginComponent },
+  { path: 'marsRover', component: MarsRoverComponent }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ CoolSpaceProject/ClientApp/src/app/save-favorite-apod.ts
@@ -0,0 +1,3 @@
+export interface SaveFavoriteApod {
+    date: string
+}
